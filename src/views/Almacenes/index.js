f224fb9e5cf6e4047a0932c46c8944fbdad31914
ferkingmonkey <40499817+ferkingmonkey@users.noreply.github.com>
2018-10-11 22:17:12 -0700

    Busqueda de almacenes

--- src/views/Almacenes/index.js
+++ src/views/Almacenes/index.js
@@ -2,11 +2,10 @@ import React, { Component } from 'react';
 //import { Button, Col, Row } from 'reactstrap';
 import { connect } from 'react-redux';
 import * as actions from '../../actions/dash.js';
-//import {api} from '../../actions/_request';
-
+import {api} from '../../actions/_request';
 
 const ListaDeAlmacenes = ({almacen}) => 
-(
+(           
     <div className="card">
         <div className="card-header">
             <strong>Almacenes</strong>
@@ -20,13 +19,13 @@ const ListaDeAlmacenes = ({almacen}) =>
                     </tr>
                 </thead>
                 <tbody>
-                {/*
-                    almacen.map((item, i) => 
+                {
+                    almacen.almacenes.map((item, i) => 
                         <tr key = { i } >
                             <td> { item.id } </td>
                             <td> { item.nombre } </td>
                         </tr>
-                    )*/
+                    )
                 }
                 </tbody>
             </table>  
@@ -44,55 +43,84 @@ class Almacenes extends Component {
                 id : '',
                 nombre : '',
             },
+            almacenes : [],
         }
-      
         //this.handleInputChange = this.handleInputChange.bind(this);
         //this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-render() {
+    componentWillMount()
+    {
+        var {almacenes} = new Array();
+        let temp = this;
 
-    let almacen = this.state;
+        api().get(`/Almacenes`)
+        .then(function(response)
+        {
+            if(response.status === 200)
+            {
+                if(response.data[0] != null)
+                {
+                    almacenes = response.data;
+                    for(var a=1; a<response.data.length; a++) { almacenes[a-1] = almacenes[a]; }
+                    almacenes[response.data.length-1]='';
+                    temp.setState({
+                        almacenes : almacenes,
+                    })
+                }
+            }
+        });
+    }
 
-      return (
-        <div className="container-fluid">
-            <div className="animated fadeIn">
-                <div className="row">
-                    <div className="col-sm-12 col-lg-6">
-                        <div className="card">
-                            <div className="card-header">
-                                <strong>Registro De Almacenes</strong>
-                            </div>
-                            <div className="card-body">
-                                <div className="row">
-                                    <div className="col-sm-12">
-                                        <div className="form-group">
-                                            <label>Folio:</label>
-                                            <input className="form-control" placeholder="autoasigned" type="text" readOnly name="folio" />
-                                        </div>
-                                        <div className="form-group">
-                                            <label>Nombre:</label>
-                                             <input className="form-control" type="text" autoFocus name="insumo" onChange = {this.handleInputChange} />
+    handleSubmit(evt){
+        evt.preventDefault();
+
+    }
+
+    render() {
+
+        let almacen = this.state;
+
+        return (
+            <div className="container-fluid">
+                <div className="animated fadeIn">
+                    <div className="row">
+                        <div className="col-sm-12 col-lg-6">
+                            <div className="card">
+                                <div className="card-header">
+                                    <strong>Registro De Almacenes</strong>
+                                </div>
+                                <div className="card-body">
+                                    <div className="row">
+                                        <div className="col-sm-12">
+                                            <div className="form-group">
+                                                <label>Folio:</label>
+                                                <input className="form-control" placeholder="autoasigned" type="text" readOnly name="folio" />
+                                            </div>
+                                            <div className="form-group">
+                                                <label>Nombre:</label>
+                                                <input className="form-control" type="text" autoFocus name="insumo" onChange = {this.handleInputChange} />
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                                <div className="row">
-                                    <div className="col-sm-12">
-                                        <div>
-                                            <button className="btn btn-block btn-primary" type="button" onClick={this.handleSubmit} >Guardar</button>
+                                    <div className="row">
+                                        <div className="col-sm-12">
+                                            <div>
+                                                <button className="btn btn-block btn-primary" type="button" onClick={this.handleSubmit} >Guardar</button>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="col-sm-12 col-lg-6">
-                        { <ListaDeAlmacenes almacen = {almacen} /> }
+                        <div className="col-sm-12 col-lg-6">
+                            { <ListaDeAlmacenes almacen = {almacen} /> }
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    );}
+        );
+    }
 }
 
 function mapStateToProps(state, ownProps) {
